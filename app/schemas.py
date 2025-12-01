edb284d296c830cd3fb6220a9c872c28bebb7769
rapith777 <rajeshpithani777@gmail.com>
2025-12-01 12:59:41 +0100

    Add Pydantic models for item schema

---
+++ app/schemas.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+from typing import Optional
+
+
+class ItemBase(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+
+
+class ItemCreate(ItemBase):
+    pass
+
+
+class ItemRead(ItemBase):
+    id: int
+
+    class Config:
+        orm_mode = True
