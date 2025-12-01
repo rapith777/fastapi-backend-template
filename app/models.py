9dbe2e56ce3d71b5db4f5a99a52fffa434bac353
rapith777 <rajeshpithani777@gmail.com>
2025-12-01 12:59:06 +0100

    Add Item model for database representation

---
+++ app/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, Float
+
+from app.db import Base
+
+
+class Item(Base):
+    __tablename__ = "items"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True, nullable=False)
+    description = Column(String, nullable=True)
+    price = Column(Float, nullable=False)
